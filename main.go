02e2c709a148ce7aa1b2b87d616f70926dfe1dd2
Kaidi Ahas <kaidiahas00@gmail.com>
2026-02-11 21:18:45 +0200

    refacture: create struct and method Add

--- main.go
+++ main.go
@@ -5,98 +5,63 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"slices"
 	"strconv"
 	"strings"
 )
 
+type Stats struct {
+	Min float64
+	Max float64
+	Sum float64
+	Count int
+}
+
+func (s *Stats) Add(value float64) {
+	if value < s.Min {
+		s.Min = value
+	}
+	if value > s.Max {
+		s.Max = value
+	}
+	s.Sum += value
+	s.Count++	
+}
+
 func main() {
-	fileHandle, err := os.Open("measurements.txt")
+	file, err := os.Open("measurements.txt")
 	if err != nil {
 		log.Println(err)
 	}
-	defer fileHandle.Close()
+	defer file.Close()
+	
+	statsByStation := make(map[string]*Stats)
 
-	stationMeasurements := make(map[string][]float64)
-
-	scanner := bufio.NewScanner(fileHandle)
+	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := scanner.Text()
-		separated := strings.Split(line, ";")
-		if len(separated) != 2 {
+		fields := strings.Split(line, ";")
+		if len(fields) != 2 {
 			log.Printf("bad line: %q", line)
 			continue
 		}
-		temp, err := strconv.ParseFloat(separated[1], 64)
+		
+		temp, err := strconv.ParseFloat(fields[1], 64)
 		if err != nil {
 			log.Println(err)
 			continue
 		}
 
-		station := separated[0]
+		stats := &Stats{}
 
-		stationMeasurements[station] = append(stationMeasurements[station], temp)
+		stats.Add(temp)
+
+		station := fields[0]
+
+		statsByStation[station] = stats
 	}
 	if err := scanner.Err(); err != nil {
 		log.Println(err)
 	}
-
-	output := make(map[string][]float64)
-
-	for station, temps := range stationMeasurements {
-		output[station] = calculate(temps)
-	}
-
-	stations := make([]string, 0, len(output))
-	for station := range output {
-		stations = append(stations, station)
-	}
-
-	slices.Sort(stations)
-
-	fmt.Print("{")
-	for i, station := range stations {
-		statistics := output[station]
-
-		if i > 0 {
-			fmt.Print(", ")
-		}
-
-		stats := struct {
-			Min float64
-			Avg float64
-			Max float64
-		}{
-			Min: statistics[0],
-			Avg: statistics[1],
-			Max: statistics[2],
-		}
-
-		fmt.Printf(
-			"%s=%.1f/%.1f/%.1f",
-			station,
-			stats.Min,
-			stats.Avg,
-			stats.Max,
-		)
-	}
 	fmt.Println("}")
 
-}
-
-func calculate(temps []float64) []float64 {
-	var stationStatistics []float64
-
-	min := slices.Min(temps)
-	max := slices.Max(temps)
-	var sum float64
-	for _, temp := range temps {
-		sum += temp
-	}
-	count := len(temps)
-
-	average := sum / float64(count)
-
-	stationStatistics = append(stationStatistics, min, average, max)
-	return stationStatistics
 }
