8d6d731a85a64e2f65365586f635799f0dabe0f2
Kaidi Ahas <kaidiahas00@gmail.com>
2026-02-15 13:29:22 +0200

    feat: create type and methods for statistics

--- main.go
+++ main.go
@@ -5,28 +5,67 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"slices"
 	"strconv"
 	"strings"
 )
 
+// Implement the Stats
+// Stats are updated by streaming
+
+// goal read the temperature measurements per weather station, aggregate the statistics and print to the standard output
+
+// Create stats
+type Stats struct {
+	Min float64
+	Max float64
+	Sum float64
+	Count int
+}
+
+// create add and average methods for stats
+func (s *Stats) Add(value float64) {
+	if s.Count == 0 {
+		s.Min = value
+		s.Max = value
+	}
+	if s.Min > value {
+		s.Min = value
+	}
+	if s.Max < value {
+		s.Max = value
+	}
+	s.Count++
+	s.Sum+=value
+}
+
+func (s *Stats) Avg() float64{
+	return s.Sum / float64(s.Count)
+}
+
 func main() {
+	// Create a separate function for producing the file
+	// name filehandle to file (file is a handle)
 	fileHandle, err := os.Open("measurements.txt")
 	if err != nil {
+		// this error needs to be fatal, without file, the program is useless
 		log.Println(err)
 	}
 	defer fileHandle.Close()
 
-	stationMeasurements := make(map[string][]float64)
+	// name it to a measurement
+	// one measurement is station and it's statistics
+	// create a type for measurement (station, temperature)
+	stationMeasurements := make(map[string]*Stats)
 
 	scanner := bufio.NewScanner(fileHandle)
 	for scanner.Scan() {
-		line := scanner.Text()
-		separated := strings.Split(line, ";")
+		// rename separated to parts or fields and use Cut instead
+		separated := strings.Split(scanner.Text(), ";")
 		if len(separated) != 2 {
-			log.Printf("bad line: %q", line)
+			log.Printf("bad line")
 			continue
 		}
+		// Create a separate function for parsing
 		temp, err := strconv.ParseFloat(separated[1], 64)
 		if err != nil {
 			log.Println(err)
@@ -35,68 +74,33 @@ func main() {
 
 		station := separated[0]
 
-		stationMeasurements[station] = append(stationMeasurements[station], temp)
+		s, exists := stationMeasurements[station]
+		if !exists {
+			s = &Stats{}
+			stationMeasurements[station] = s
+		}
+		s.Add(temp)
 	}
 	if err := scanner.Err(); err != nil {
 		log.Println(err)
 	}
 
-	output := make(map[string][]float64)
 
-	for station, temps := range stationMeasurements {
-		output[station] = calculate(temps)
+	// I need a value from that address
+	stats := *stationMeasurements["Hamburg"]
+
+	avg := stats.Avg()
+
+	var stations []string
+
+	for s := range stationMeasurements {
+		stations = append(stations, s)
 	}
 
-	stations := make([]string, 0, len(output))
-	for station := range output {
-		stations = append(stations, station)
-	}
 
-	slices.Sort(stations)
-
-	fmt.Print("{")
-	for i, station := range stations {
-		statistics := output[station]
-
-		if i > 0 {
-			fmt.Print(", ")
-		}
-
-		stats := struct {
-			Min float64
-			Avg float64
-			Max float64
-		}{
-			Min: statistics[0],
-			Avg: statistics[1],
-			Max: statistics[2],
-		}
-
-		fmt.Printf(
-			"%s=%.1f/%.1f/%.1f",
-			station,
-			stats.Min,
-			stats.Avg,
-			stats.Max,
-		)
+	fmt.Println("{")
+	for station, stat := range stationMeasurements {
+		fmt.Printf("%s=%.1f/%.1f/%.1f,\n", station, stat.Min, avg, stat.Max)
 	}
 	fmt.Println("}")
-
-}
-
-func calculate(temps []float64) []float64 {
-	var stationStatistics []float64
-
-	min := slices.Min(temps)
-	max := slices.Max(temps)
-	var sum float64
-	for _, temp := range temps {
-		sum += temp
-	}
-	count := len(temps)
-
-	average := sum / float64(count)
-
-	stationStatistics = append(stationStatistics, min, average, max)
-	return stationStatistics
 }
