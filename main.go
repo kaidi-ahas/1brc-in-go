809537974c54530bb83534e77feb2f14e79f128d
Kaidi Ahas <kaidiahas00@gmail.com>
2026-02-15 11:27:53 +0200

    going back to a previous version

--- main.go
+++ main.go
@@ -1,132 +1,102 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
+	"slices"
 	"strconv"
 	"strings"
 )
 
-// Goal: Parse one line
-// create a struct for measurement
-// change countLines to return the lines slice
-
 func main() {
-	line := "Hamburg;12.3"
-	m, err := parseLine(line)
+	fileHandle, err := os.Open("measurements.txt")
 	if err != nil {
-		fmt.Println("error:", err)
-		return
+		log.Println(err)
+	}
+	defer fileHandle.Close()
+
+	stationMeasurements := make(map[string][]float64)
+
+	scanner := bufio.NewScanner(fileHandle)
+	for scanner.Scan() {
+		line := scanner.Text()
+		separated := strings.Split(line, ";")
+		if len(separated) != 2 {
+			log.Printf("bad line: %q", line)
+			continue
+		}
+		temp, err := strconv.ParseFloat(separated[1], 64)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+
+		station := separated[0]
+
+		stationMeasurements[station] = append(stationMeasurements[station], temp)
+	}
+	if err := scanner.Err(); err != nil {
+		log.Println(err)
 	}
 
-	fmt.Printf("%+v\n", m)
+	output := make(map[string][]float64)
+
+	for station, temps := range stationMeasurements {
+		output[station] = calculate(temps)
+	}
+
+	stations := make([]string, 0, len(output))
+	for station := range output {
+		stations = append(stations, station)
+	}
+
+	slices.Sort(stations)
+
+	fmt.Print("{")
+	for i, station := range stations {
+		statistics := output[station]
+
+		if i > 0 {
+			fmt.Print(", ")
+		}
+
+		stats := struct {
+			Min float64
+			Avg float64
+			Max float64
+		}{
+			Min: statistics[0],
+			Avg: statistics[1],
+			Max: statistics[2],
+		}
+
+		fmt.Printf(
+			"%s=%.1f/%.1f/%.1f",
+			station,
+			stats.Min,
+			stats.Avg,
+			stats.Max,
+		)
+	}
+	fmt.Println("}")
 
 }
 
-type Measurement struct {
-	Station string
-	Temperature float64
-}
+func calculate(temps []float64) []float64 {
+	var stationStatistics []float64
 
-func parseLine(line string) (Measurement, error) {
-	station, value, found := strings.Cut(line, ";")
-	if !found {
-		return Measurement{}, fmt.Errorf("Bad line: %q", line)
+	min := slices.Min(temps)
+	max := slices.Max(temps)
+	var sum float64
+	for _, temp := range temps {
+		sum += temp
 	}
+	count := len(temps)
 
-	temperature, err := strconv.ParseFloat(value, 64)
-	if err != nil {
-		return Measurement{}, fmt.Errorf("invalid temperature: %q: %w", value, err)
-	}
+	average := sum / float64(count)
 
-	return Measurement{
-		Station: station,
-		Temperature: temperature,
-	}, nil
+	stationStatistics = append(stationStatistics, min, average, max)
+	return stationStatistics
 }
-
-
-
-
-
-
-// import (
-// 	"bufio"
-// 	"fmt"
-// 	"log"
-// 	"os"
-// 	"strconv"
-// 	"strings"
-// )
-
-// type Stats struct {
-// 	Min float64
-// 	Max float64
-// 	Sum float64
-// 	Count int
-// }
-
-// func (s *Stats) Add(value float64) {
-
-// 	if s.Count == 0 {
-// 		s.Min = value
-// 		s.Max = value
-// 	} else {
-// 		if value < s.Min {
-// 			s.Min = value
-// 		}
-// 		if value > s.Max {
-// 			s.Max = value
-// 		}
-// 	}
-// 	s.Sum += value
-// 	s.Count++
-// }
-
-// func main() {
-// 	fmt.Println(readMeasurements("measurements.txt"))
-
-// }
-
-// func readMeasurements(path string) map[string]*Stats {
-// 	file, err := os.Open(path)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	defer file.Close()
-	
-// 	statsByStation := make(map[string]*Stats)
-
-// 	scanner := bufio.NewScanner(file)
-// 	for scanner.Scan() {
-// 		line := scanner.Text()
-// 		station, value, found := strings.Cut(line, ";")
-// 		if !found {
-// 			log.Printf("bad line: %q", line)
-// 			continue
-// 		}
-		
-// 		temperature, err := strconv.ParseFloat(value, 64)
-// 		if err != nil {
-// 			log.Println(err)
-// 			continue
-// 		}
-
-// 		stats, exists := statsByStation[station]
-
-// 		if !exists {
-// 			stats = &Stats{}
-// 			statsByStation[station]=stats
-// 		}
-
-// 		stats.Add(temperature)
-
-// 	}
-// 	if err := scanner.Err(); err != nil {
-// 		log.Println(err)
-// 	}
-// 	return statsByStation
-// }
-
-// func printStats(stats map[string]*Stats) {
-	
-// }
